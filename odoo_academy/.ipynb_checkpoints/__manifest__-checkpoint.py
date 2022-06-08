cad1db3583b5f86b40d5aabc17de931c552732b5
Alejandro Cisneros Pérez <acisneros@adicom.com.mx>
2022-06-08 15:36:00 +0000

    :sparkles: [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-o -*-
+
+{
+    "name": "Odoo Academy",
+    
+    "summary": """Academy app to manage Training""",
+    
+    "description": """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    "author": "Odoo",
+    
+    "website": "https://www.odoo.com",
+    
+    "category": "Training",
+    
+    "version": "0.1",
+    
+    "depends": ["base"],
+    
+    "data": [
+        
+        
+    ],
+    
+    "demo": [
+        
+        
+    ],
+}
